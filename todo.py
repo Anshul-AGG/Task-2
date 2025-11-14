52d766b2ddb87af30555834049d925f87aee6924
Anshul <anshulagarwal393@gmail.com>
2025-11-14 15:30:56 +0530

    Completed

--- todo.py
+++ todo.py
@@ -1,9 +1,3 @@
-"""Hints/Mini Guide:
-1.Use lists to store tasks
-2.Implement add/remove/view functionality
-3.Store tasks in a text file using open()"""
-
-
 class ToDoList:
 
     def __init__(self, task=None):
@@ -14,23 +8,23 @@ class ToDoList:
     def add(self, new_task):
         if isinstance(new_task, str) and new_task.strip():
             self.task.append(new_task.strip())
-            print("task added", new_task.strip())
+            print("task added, ", new_task.strip())
         else:
             print("Invalid Input")
 
     def remove(self, task_name):
         try:
             self.task.remove(task_name.strip())
-            print("task is removed", task_name.strip())
+            print("task is removed, ", task_name.strip())
         except ValueError:
             print(f"error: {task_name.strip()} not found in the list")
 
     def view(self):
         print("Current tasks are: ")
         for index, task in enumerate(self.task):
-            print(index + 1, task)
+            print(f"{index + 1}. {task}")
 
-    def file(self, filename="tasks.txt"):
+    def save_file(self, filename="tasks.txt"):
         try:
             with open(filename, "w") as f:
                 for task in self.task:
@@ -41,10 +35,10 @@ class ToDoList:
 
 
 my_study_list = ToDoList("Start Apna College AI/ML course")
-
+my_study_list.view()
 my_study_list.add("Read chapter 1 on Python classes")
 my_study_list.add("Set up virtual environment")
 my_study_list.add("Review previous notes on SailPoint and Azure")
 my_study_list.remove("Review previous notes on SailPoint and Azure")
 my_study_list.view()
-my_study_list.file()
+my_study_list.save_file()
